88487a93aa0a475a280af265fe3a8ec1c65b974d
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-10 02:25:11 -0300

    Ponto Turistico List criado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import AtividadeList from './pages/AtividadeList';
+import PontoTuristicoList from './pages/PontoTuristicoList';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/atividades" component={AtividadeList} />
+        <Route exact path="/pontos-turisticos" component={PontoTuristicoList} />
        
       </Switch>
     </Router>
---
+++ frontend/src/pages/PontoTuristicoList.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../CSS/Listagem.css';
+import { Link } from 'react-router-dom';
+import Nav from '../components/Nav'; 
+
+
+function PontoTuristicoList() {
+    const [pontosTuristicos, setPontosTuristicos] = useState([]);
+  
+    useEffect(() => {
+      axios.get('http://127.0.0.1:5000/api/pontos_turisticos').then(response => {
+          setPontosTuristicos(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+  
+    const limitarDescricao = (descricao) => {
+      const limite = 200;
+      if (descricao.length > limite) {
+        return descricao.slice(0, limite) + '...';
+      }
+      return descricao;
+    }
+  
+    return (
+      <div className="pt-list">
+         <header>
+        <Nav /> 
+      </header>
+        <h1>Pontos turísticos</h1>
+        {pontosTuristicos.map(pontoTuristico => (
+          <div className="pt-card" key={pontoTuristico.id}>
+            <img src={pontoTuristico.url_imagem_1} alt={pontoTuristico.nome} />
+            <div className="card-body">
+              <h2>{pontoTuristico.nome}</h2>
+              <p>{limitarDescricao(pontoTuristico.descricao)}</p>
+              <p>{pontoTuristico.localizacao}</p>
+              <Link to={`/pontos-turisticos/${pontoTuristico.id}`} className="cta-btn">Ver mais</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default PontoTuristicoList;
