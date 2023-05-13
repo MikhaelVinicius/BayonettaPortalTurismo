d1364fb2b4034c9e26dc52ebeab7bf4fd95a648b
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-13 06:44:38 -0300

    carrosel adicionado a atividades

--- frontend/src/pages/AtividadeDetalhes.js
+++ frontend/src/pages/AtividadeDetalhes.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import '../CSS/Detalhes.css';
 import Nav from '../components/Nav'; 
 import Comentarios from '../components/comentarios';
 
+import $ from 'jquery';
+
+
+import  '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import '../../node_modules/slick-carousel/slick/slick.js';
 function AtividadeDetalhes(props) {
   const [atividade, setAtividade] = useState(null);
+  const slickRef = useRef(null);
+  
 
   useEffect(() => {
     axios.get(`http://127.0.0.1:5000/api/atividades/${props.match.params.id}`).then(response => {
@@ -15,6 +23,22 @@ function AtividadeDetalhes(props) {
         console.log(error);
       });
   }, [props.match.params.id]);
+
+  useEffect(() => {
+    if (slickRef.current && atividade) {
+      const initSlick = () => {
+        $(".slider").not('.slick-initialized').slick({
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      };
+
+      setTimeout(initSlick, 100);
+    }
+  }, [slickRef, atividade]);
 
   if (!atividade) {
     return <div>Carregando...</div>;
@@ -26,7 +50,14 @@ function AtividadeDetalhes(props) {
         <Nav /> 
       </header>
       <div className="imagem">
-        <img src={atividade.url_imagem_1} alt={atividade.nome} />
+      <div className="slider" ref={slickRef}>
+          <div>
+            <img src={atividade.url_imagem_1} alt={atividade.nome} />
+          </div>
+          <div>
+            <img src={atividade.url_imagem_2} alt={atividade.nome} />
+          </div>
+        </div>
         <p>Telefone: {atividade.telefone}</p>
         <p>Email: {atividade.email}</p>
         <p>Endereço: {atividade.endereco}</p>
