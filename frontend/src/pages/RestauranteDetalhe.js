6eb1751d0bf41d714baf57145cd8e10087ad8d5b
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-13 06:41:55 -0300

    Carrosel de imagens adicionado em restaurantes

--- frontend/src/pages/RestauranteDetalhe.js
+++ frontend/src/pages/RestauranteDetalhe.js
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import '../CSS/Detalhes.css';
 import Nav from '../components/Nav'; 
 import Comentarios from '../components/comentarios';
 
+import $ from 'jquery';
+
+
+import  '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import '../../node_modules/slick-carousel/slick/slick.js';
+
+
 function RestauranteDetalhe(props) {
   const [restaurante, setRestaurante] = useState(null);
+  const slickRef = useRef(null);
 
   useEffect(() => {
     axios.get(`http://127.0.0.1:5000/api/restaurantes/${props.match.params.id}`).then(response => {
@@ -15,6 +24,22 @@ function RestauranteDetalhe(props) {
         console.log(error);
       });
   }, [props.match.params.id]);
+
+  useEffect(() => {
+    if (slickRef.current && restaurante) {
+      const initSlick = () => {
+        $(".slider").not('.slick-initialized').slick({
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      };
+
+      setTimeout(initSlick, 100);
+    }
+  }, [slickRef, restaurante]);
 
   if (!restaurante) {
     return <div>Carregando...</div>;
@@ -26,7 +51,14 @@ function RestauranteDetalhe(props) {
         <Nav /> 
       </header>
       <div className="imagem">
-        <img src={restaurante.imagem1} alt={restaurante.nome} />
+      <div className="slider" ref={slickRef}>
+          <div>
+            <img src={restaurante.imagem1} alt={restaurante.nome} />
+          </div>
+          <div>
+            <img src={restaurante.imagem2} alt={restaurante.nome} />
+          </div>
+        </div>
         <p>Telefone: {restaurante.telefone}</p>
         <p>Email: {restaurante.email}</p>
         <p>Endereço: {restaurante.endereco}</p>
