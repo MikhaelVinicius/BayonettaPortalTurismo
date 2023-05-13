2a4c0ea348c1bcb7246807a700542fc1da6367f1
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-13 06:38:34 -0300

    Carrosel em hospedagens adicionado

--- frontend/src/pages/HospedagemDetalhe.js
+++ frontend/src/pages/HospedagemDetalhe.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import '../CSS/Detalhes.css';
 import Nav from '../components/Nav'; 
 import Comentarios from '../components/comentarios';
+import $ from 'jquery';
+
+import  '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import '../../node_modules/slick-carousel/slick/slick.js';
 
 function HospedagemDetalhes(props) {
   const [hospedagem, setHospedagem] = useState(null);
+   const slickRef = useRef(null);
 
   useEffect(() => {
     axios.get(`http://127.0.0.1:5000/api/hospedagens/${props.match.params.id}`).then(response => {
@@ -15,6 +21,22 @@ function HospedagemDetalhes(props) {
         console.log(error);
       });
   }, [props.match.params.id]);
+
+    useEffect(() => {
+    if (slickRef.current && hospedagem) {
+      const initSlick = () => {
+        $(".slider").not('.slick-initialized').slick({
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      };
+
+      setTimeout(initSlick, 100);
+    }
+  }, [slickRef, hospedagem]);
 
   if (!hospedagem) {
     return <div>Carregando...</div>;
@@ -26,7 +48,14 @@ function HospedagemDetalhes(props) {
         <Nav /> 
       </header>
       <div className="imagem">
-        <img src={hospedagem.url_imagem_1} alt={hospedagem.nome} />
+       <div className="slider" ref={slickRef}>
+          <div>
+            <img src={hospedagem.url_imagem_1} alt={hospedagem.nome} />
+          </div>
+          <div>
+            <img src={hospedagem.url_imagem_2} alt={hospedagem.nome} />
+          </div>
+        </div>
         <p>Estrelas: {hospedagem.estrelas}</p>
         <p>Tipo: {hospedagem.tipo}</p>
         <p>Website: {hospedagem.website}</p>
