ed4cb25cdf635f00404326477e907100756ef671
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-24 18:19:02 -0300

    Carrosel em publi implementado

--- frontend/src/components/cardPubli.js
+++ frontend/src/components/cardPubli.js
@@ -1,48 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './cardPubli.css';
 import { Link } from 'react-router-dom';
+import $ from 'jquery';
+import  '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import '../../node_modules/slick-carousel/slick/slick.js';
+import Slider from 'react-slick';
+
 
 
 
 function CardPubli() {
-    const [publis, setPublis] = useState([]);
-  
-    useEffect(() => {
-      axios.get('http://127.0.0.1:5000/api/publi').then(response => {
-          setPublis(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, []);
-  
-     
-    const limitarDescricao = (descricao) => {
-        const limite = 100;
-        if (descricao.length > limite) {
-          return descricao.slice(0, limite) + '...';
-        }
-        return descricao;
-      }
-  
-    return (
-        <div className="list">
-          
-          {publis.map(publi => (
-            <div className="card" key={publi.id}>
-              <img src={publi.imagem1} alt={publi.nome} />
-              <div className="cardbody">
-                <h2>{publi.nome}</h2>
-                <p>{limitarDescricao(publi.descricao)}</p>
-                
-                <Link to={`/publi/${publi.id}`} className="ctabtn">Ver mais</Link>
-              </div>
-            </div>
-          ))}
-        
-        </div>
-      );
+  const [publis, setPublis] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api/publi')
+      .then(response => {
+        setPublis(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const limitarDescricao = (descricao) => {
+    const limite = 100;
+    if (descricao.length > limite) {
+      return descricao.slice(0, limite) + '...';
     }
-  
-  export default CardPubli;
+    return descricao;
+  };
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...sliderSettings}>
+      {publis.map(publi => (
+        <div className="card" key={publi.id}>
+          <img src={publi.imagem1} alt={publi.nome} />
+          <div className="cardbody">
+            <h2>{publi.nome}</h2>
+            <p>{limitarDescricao(publi.descricao)}</p>
+            <Link to={`/publi/${publi.id}`} className="ctabtn">
+              Ver mais
+            </Link>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+}
+
+export default CardPubli;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -7,6 +7,7 @@ import PontoTuristicoList from './PontoTuristicoList';
 import Footer from '../components/footer';
 import CardPubli from '../components/cardPubli';
 
+
 function Homepage() {
 
   const pontoTuristicoListRef = useRef(null);
@@ -31,7 +32,7 @@ function Homepage() {
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://mediaim.expedia.com/destination/1/255802739e639a1f610362ba2a9b30e8.jpg" alt="First slide"/>
+      <img  class="d-block w-100 blur" src="https://mediaim.expedia.com/destination/1/255802739e639a1f610362ba2a9b30e8.jpg" alt="First slide"/>
       <div class="carousel-caption d-none d-md-block">
         <div class="card-publi">
         <CardPubli/>
