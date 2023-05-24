b785dc057ad52ab4b4b276c44ea725f0c0bea778
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-24 18:44:08 -0300

    Autoplay implementado

--- frontend/src/components/cardPubli.js
+++ frontend/src/components/cardPubli.js
@@ -33,11 +33,13 @@ function CardPubli() {
   };
 
   const sliderSettings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, 
     responsive: [
       {
         breakpoint: 768,
